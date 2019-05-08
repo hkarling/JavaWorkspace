f179641bd516557375b8c23a001bdef812fa2015
hkarling <dantert@gmail.com>
2019-05-08 20:53:32 +0900

    05082100

--- 20190508/array/MemoryApart.java
+++ 20190508/array/MemoryApart.java
@@ -1,19 +1,29 @@
-class MultiArray{
-//정수형 2차원 배열 8*9
-	
-//메소드이름 :array99
-	
-	//for loop 를 사용하여 
-	//배열에 곱한 (구구단)결과저장
-	//배열에 결과를 꺼내출력
-		
-	
-		
-		
-}
-public class MemoryApart{
-//main메소드에서 
-//MultiArray객체의 array99메소드호출
-	
+class MultiArray {
+	// 정수형 2차원 배열 8*9
+	int[][] timesTable = new int[9][8];
+
+	// 메소드이름 :array99
+	public void array99() {
+		// for loop 를 사용하여 배열에 곱한 (구구단) 결과 저장
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 8; j++) {
+				timesTable[i][j] = (i + 1) * (j + 2);
+			}
+		}
+		// 배열에 결과를 꺼내출력
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 8; j++)
+				System.out.print((j + 2) + " * " + (i + 1) + " = " + timesTable[i][j] + "\t");
+			System.out.println();
+		}
+	}
 }
 
+public class MemoryApart {
+	// main메소드에서
+	public static void main(String[] args) {
+		// MultiArray객체의 array99메소드호출
+		MultiArray obj = new MultiArray();
+		obj.array99();
+	}
+}
--- 20190508/array/ObjectInArray.java
+++ 20190508/array/ObjectInArray.java
@@ -1,21 +1,34 @@
 //ObjectInArray.java
-
-class ObjectOne{
+class ObjectOne {
 	public int a;
 }
 
-public class ObjectInArray{
-	//메인 메소드에서 
-	
-		//ObjectOne 타입을 5개 까지 저장하는 배열을 선언하고
-		 
-		//5개의 ObjectOne객체를 생성하세요.
-		//각 객체가 갖고있는 전역변수 출력
-		//각 객체의 주소값 출력
-			
-		//각각의 ObjectOne객체가 서로 다른 a의 값을 갖도록 a의 값을 변경하세요.			
-		//각 ObjectOne 객체의 a를 출력하세요
-			
+public class ObjectInArray {
+	// 메인 메소드에서
+	public static void main(String[] args) {
+		// ObjectOne 타입을 5개 까지 저장하는 배열을 선언하고
+		ObjectOne[] objectOnes = new ObjectOne[5];
+
+		// 5개의 ObjectOne객체를 생성하세요.
+		for (int i = 0; i < 5; i++)
+			objectOnes[i] = new ObjectOne();
 		
-	
+		// 각 객체가 갖고있는 전역변수 출력
+		System.out.println("----- 각 객체의 전역변수 -----");
+		for (ObjectOne obj : objectOnes)
+			System.out.println("전역변수: " + obj.a);
+		// 각 객체의 주소값 출력
+		System.out.println("----- 각 객체의 주소값 -----");
+		for (ObjectOne obj : objectOnes)
+			System.out.println("주소값: " + obj);
+
+		// 각각의 ObjectOne객체가 서로 다른 a의 값을 갖도록 a의 값을 변경하세요.
+		for (int i = 0; i < 5; i++) {
+			objectOnes[i].a = (int) (Math.random() * 10);
+		}
+		// 각 ObjectOne 객체의 a를 출력하세요
+		System.out.println("----- 각 객체의 전역변수 -----");
+		for (ObjectOne obj : objectOnes)
+			System.out.println("전역변수: " + obj.a);
+	}
 }
