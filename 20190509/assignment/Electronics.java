bd366eb2d03fa4df7620b8e6bb73b8e6748d66f0
hkarling <dantert@gmail.com>
2019-05-09 18:43:59 +0900

    05091900

---
+++ 20190509/assignment/Electronics.java
@@ -0,0 +1,45 @@
+/**
+ * 이름: 윤참솔 날짜: 5월 9일 주제: Electronics 클래스
+ */
+public class Electronics {
+    private int modelNo;
+    private String modelName;
+    private int modelPrice;
+    private String modelDetail;
+
+    //////// 생성자 ////////
+    public Electronics(){ }
+
+    public Electronics(int modelNo, String modelName, int modelPrice, String modelDetail) {
+        this.modelNo = modelNo;
+        this.modelName = modelName;
+        this.modelPrice = modelPrice;
+        this.modelDetail = modelDetail;
+    }
+
+    //////// Setter, Getter ////////
+    public void setModelDetail(String modelDetail) {
+        this.modelDetail = modelDetail;
+    }
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+    public void setModelNo(int modelNo) {
+        this.modelNo = modelNo;
+    }
+    public void setModelPrice(int modelPrice) {
+        this.modelPrice = modelPrice;
+    }
+    public String getModelDetail() {
+        return modelDetail;
+    }
+    public String getModelName() {
+        return modelName;
+    }
+    public int getModelNo() {
+        return modelNo;
+    }
+    public int getModelPrice() {
+        return modelPrice;
+    }
+}
---
+++ 20190509/constructor/StudentService.java
@@ -0,0 +1,60 @@
+/**
+ * Service는 Business Logic을 담당하는 클래스 (업무를 처리하는 객체)
+ */
+public class StudentService {
+    Student[] arrStudent;
+
+    // 학생정보 등록하기
+    void insert(String[][] data) {
+
+        arrStudent = new Student[data.length];
+
+        for (int i = 0; i < 5; i++) {
+            arrStudent[i] = this.creatStudent(data[i]);
+        }
+    }
+
+    // Student 객체를 생성하여 값을 설정하고 반환
+    private Student creatStudent(String[] info) {
+
+        /*Student student = new Student();
+        student.setName(info[0]);
+        student.setAge(Integer.parseInt(info[1]));
+        student.setAddress(info[2]);
+        */
+        Student student = new Student(info[0], Integer.parseInt(info[1]), info[2]);
+        return student;
+    }
+
+    // 학생정보 출력하기
+    public void printTable() {
+        System.out.println("--- 모든 학생 list(" + arrStudent.length + "명) ---");
+        for (Student s : arrStudent) {
+            System.out.println(s.getName() + "\t" + s.getAge() + "\t" + s.getAddress());
+        }
+    }
+    // 학생 이름에 해당하는 정보 검색
+    public void searchByName(String name) {
+        for(Student student : arrStudent) {
+            if(student.getName().equals(name)){
+                System.out.println(student.getName() + "\t" + student.getAge() + "\t" + student.getAddress());
+                return;
+            }
+        }
+        System.out.println(name + " 학생의 정보가 없습니다.");
+    }
+    // 학생 이름에 해당하는 나이와 주소 변경
+    public void update(Student student) {
+        for(Student st : arrStudent) {
+            if(student.getName().equals(student.getName())){
+                st.setName(student.getName());
+                st.setAge(student.getAge());
+                st.setAddress(student.getAddress());
+
+                
+                System.out.println(student.getName() + "\t" + student.getAge() + "\t" + student.getAddress());
+                return;
+            }
+        }
+    }
+}
