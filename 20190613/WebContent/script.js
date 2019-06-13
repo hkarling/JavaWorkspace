6bd7c1ead0be7680b9fdfb365de29b7c04319db7
hkarling <dantert@gmail.com>
2019-06-13 18:22:47 +0900

    06131820

---
+++ 20190613/WebContent/script.js
@@ -0,0 +1,16 @@
+//	alert("출력해라.");
+document.write("<h1 style='color:blue'>졸지말라우</h1>");
+	
+// 마우스를 오버 했을때 기능
+function test1(th) {
+	th.style.backgroundColor='red';
+}
+// 마우스를 아웃 했을때 기능
+function test2(th) {
+	th.style.backgroundColor='white';		
+}
+// 클릭했을때 기능
+function test3() {
+	document.bgColor='pink';
+	alert("신난다고해라");
+}
